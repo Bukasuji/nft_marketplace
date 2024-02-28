eaf89f355577ee52e43c1aaca087e3053f2e96e9
Chukwuebuka Osuji <74817012+Bukasuji@users.noreply.github.com>
2024-02-28 19:37:45 +0100

    added hooks

--- lib/hook.ts
+++ lib/hook.ts
@@ -1,7 +1,8 @@
-import { useDispatch, useSelector, useStore } from 'react-redux';
+// app/hooks.ts
+import { useDispatch, useSelector } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch, AppStore } from './store';
+import type { RootState, AppDispatch } from './store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppStore: () => AppStore = useStore;
+
